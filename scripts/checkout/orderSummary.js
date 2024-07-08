c1b42aadbff0fd299bef17e192c6ec88e658dcce
jecraftx <rickyssson783@gmail.com>
2024-07-09 05:50:51 +0900

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -28,6 +28,7 @@ export function renderOrderSummary() {
 
         cartSummaryHTML += `
         <div class="cart-item-container 
+            js-cart-item-container
             js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: ${dateString};
@@ -44,14 +45,16 @@ export function renderOrderSummary() {
                 <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity 
+                    js-product-quantity-${matchingProduct.id}">
                     <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                     </span>
                     <span class="update-quantity-link link-primary">
                     Update
                     </span>
-                    <span class="delete-quantity-link link-primary js-delete-link" 
+                    <span class="delete-quantity-link link-primary js-delete-link
+                                js-delete-link-${matchingProduct.id}" 
                     data-product-id="${matchingProduct.id}">
                     Delete
                     </span>
@@ -111,10 +114,10 @@ export function renderOrderSummary() {
                 const productId = link.dataset.productId;
                 removeFromCart(productId);
 
-                // const container = document.querySelector( `.js-cart-item-container-${productId}`);
-                // container.remove();
+                const container = document.querySelector( `.js-cart-item-container-${productId}`);
+                container.remove();
 
-                renderPaymentSummary();
+                // renderPaymentSummary();
                 renderOrderSummary()
             });
         });
